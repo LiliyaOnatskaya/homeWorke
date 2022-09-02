028a0247cc00d3a3ed7df7e903bbe19bdbfcc083
Liliya Onatskaya <liliyaonatskaya@gmail.com>
2022-09-02 16:58:23 +0200

    Home_work3_column_print

--- lesson2_30.08/hw3.py
+++ lesson2_30.08/hw3.py
@@ -1,9 +1,11 @@
 digit=int(input('Enter 4 digit number - '))
-a=digit//1000%10
-b=digit//100%10
-c=digit//10%10
-d=digit%10
-print(a)
-print(b)
-print(c)
+
+digit,a=divmod(digit,10)
+digit,b=divmod(digit,10)
+digit,c=divmod(digit,10)
+digit,d=divmod(digit,10)
+
 print(d)
+print(c)
+print(b)
+print(a)
